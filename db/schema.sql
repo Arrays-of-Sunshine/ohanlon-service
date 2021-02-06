d696b40b418aba29c1072869229491dbb70304e3
Joshua OHanlon <josh.t.ohanlon@gmail.com>
2021-02-06 17:56:17 -0600

    Updated seeding script to use one intermediate table

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,32 @@
 
 \c sdc;
 
+CREATE TABLE intermediate (
+  id INT,
+  items_title TEXT,
+  revRec BOOLEAN,
+  revUser Text,
+  revDate TEXT,
+  revVerified BOOLEAN,
+  revHelpful INT,
+  revNotHelpful INT,
+  revDes TEXT,
+  rating INT,
+  ratingRev INT,
+  tag TEXT,
+  tagRating INT,
+  images TEXT,
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE Items (
-  items_id INT GENERATED ALWAYS AS IDENTITY,
+  items_id INT,
   title TEXT NOT NULL,
   PRIMARY KEY  (items_id)
 );
 
 CREATE TABLE Review (
-  review_id INT GENERATED ALWAYS AS IDENTITY,
+  review_id INT,
   recommendation BOOLEAN NOT NULL,
   revUser TEXT NOT NULL,
   revDate TEXT NOT NULL,
@@ -24,7 +42,7 @@ CREATE TABLE Review (
 );
 
 CREATE TABLE Ratings (
-  ratings_id INT GENERATED ALWAYS AS IDENTITY,
+  ratings_id INT,
   rating INT NOT NULL,
   revNum INT NOT NULL,
   items_id INT,
@@ -39,9 +57,9 @@ CREATE TABLE Ratings (
 );
 
 CREATE TABLE Tags (
-  tags_id INT GENERATED ALWAYS AS IDENTITY,
-  tag TEXT[],
-  tagRating INT[],
+  tags_id INT,
+  tag TEXT,
+  tagRating INT,
   review_id INT,
   PRIMARY KEY(tags_id),
   CONSTRAINT fk_review
@@ -50,8 +68,8 @@ CREATE TABLE Tags (
 );
 
 CREATE TABLE Images (
-  images_id INT GENERATED ALWAYS AS IDENTITY,
-  images TEXT[],
+  images_id INT,
+  images TEXT,
   review_id INT,
   PRIMARY KEY (images_id),
   CONSTRAINT fk_review

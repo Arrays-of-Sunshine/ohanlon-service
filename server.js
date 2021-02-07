4300bcd583d7d42764dc28b8e62366cff7a85dfb
Joshua OHanlon <josh.t.ohanlon@gmail.com>
2021-02-06 19:36:04 -0600

    Set up basic query to test response times.

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const pool = require('./db/index.js');
 
 const app = express();
 
+app.get('/products' , (req, res) => {
+  pool.query(`EXPLAIN ANALYZE SELECT * FROM intermediate WHERE id = 121`, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(data);
+    }
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
